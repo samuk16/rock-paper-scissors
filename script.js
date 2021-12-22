300518b8654c00fc6649d80ee17ef730ffb4e808
Samuk <marcelolamas19@gmail.com>
2021-12-22 06:01:19 -0300

    firsts changes on index.html and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let playerSelection = prompt("Enter your election");
+const randomSelec = ["rock", "paper", "scissors"];
+
+function computerPlay(randomSelec) {
+    Math.floor(Math.random() * 3) + 1;
+
+}
+console.log(computerPlay())
